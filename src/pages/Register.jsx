35113785ffce9e83168f8a24f064d1eff4cc2e78
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2025-01-06 10:13:23 +0500

    new

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -76,9 +76,9 @@ function Register() {
 
         <div className="my-5 flex flex-col gap-3">
           <button className="btn btn-warning btn-block">Register</button>
-          <button type="button" className="btn btn-secondary btn-block ">
+          {/* <button type="button" className="btn btn-secondary btn-block ">
             Google
-          </button>
+          </button> */}
         </div>
 
         <div className="text-center ">
